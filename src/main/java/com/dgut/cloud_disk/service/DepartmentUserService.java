e0532ef7c1b8d95948664c49d715ee9e8b41dde0
huangweijian_fellett <535363313@qq.com>
2020-09-18 16:49:27 +0800

    first3

---
+++ src/main/java/com/dgut/cloud_disk/service/DepartmentUserService.java
@@ -0,0 +1,9 @@
+package com.dgut.cloud_disk.service;
+
+import com.dgut.cloud_disk.pojo.DepartmentUser;
+
+import java.util.List;
+
+public interface DepartmentUserService {
+    public List<DepartmentUser> allUser();
+}
