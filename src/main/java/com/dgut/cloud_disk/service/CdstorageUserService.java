f413e3f97d616a174dd5ae25ecffb5604fd377f3
z1025407412 <59383563+z1025407412@users.noreply.github.com>
2020-09-23 10:13:21 +0800

    9/23

--- src/main/java/com/dgut/cloud_disk/service/CdstorageUserService.java
+++ src/main/java/com/dgut/cloud_disk/service/CdstorageUserService.java
@@ -3,5 +3,7 @@ package com.dgut.cloud_disk.service;
 import com.dgut.cloud_disk.pojo.CdstorageUser;
 
 public interface CdstorageUserService {
-    CdstorageUser queryByUserName(String userName);
+    CdstorageUser queryByUserMobie(String userName);
+    boolean updateUserPassword(String userPhone,String userPassword);
+    boolean updateUser(CdstorageUser user);
 }
--- src/main/java/com/dgut/cloud_disk/service/impl/CdstorageUserServiceImpl.java
+++ src/main/java/com/dgut/cloud_disk/service/impl/CdstorageUserServiceImpl.java
@@ -12,16 +12,46 @@ public class CdstorageUserServiceImpl implements CdstorageUserService {
     CdstorageUserMapper userMapper;
 
     /**
-     *根据用户名查询用户
-     * @param userName
+     *根据手机号查询用户
+     * @param userMobie
      * @return user 查询结果
      */
     @Override
-    public CdstorageUser queryByUserName(String userName) {
+    public CdstorageUser queryByUserMobie(String userMobie) {
         Example example = new Example(CdstorageUser.class);
         Example.Criteria criteria = example.createCriteria();
-        criteria.andEqualTo("userName",userName);
+        criteria.andEqualTo("userMobie",userMobie);
         CdstorageUser user = userMapper.selectOneByExample(example);
         return user;
+    }
+
+    /**
+     * 根据手机号修改密码
+     * @param userPhone 手机号
+     * @param userPassword 新密码
+     * @return 修改成功true，失败false
+     */
+    @Override
+    public boolean updateUserPassword(String userPhone, String userPassword) {
+        CdstorageUser user = new CdstorageUser();
+        user.setUserPassword(userPassword);
+        Example example = new Example(CdstorageUser.class);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("userMobie",userPhone);
+        if(userMapper.updateByExampleSelective(user, example) == 1) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean updateUser(CdstorageUser user) {
+        Example example = new Example(CdstorageUser.class);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("userWorkId",user.getUserWorkId());
+        if(userMapper.updateByExampleSelective(user, example) == 1) {
+            return true;
+        }
+        return false;
     }
 }
